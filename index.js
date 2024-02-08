af9b690d964cbcc4a1e59e411a57dd252481c194
sparky9043 <sparky9043@gmail.com>
2024-02-08 11:27:42 +0900

    Remove console.log and add player display update

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-// Button Functions with event listeners
+// Button Functions
 const rockBtn = document.querySelector('#rock-btn');
 const paperBtn = document.querySelector('#paper-btn');
 const scissorsBtn = document.querySelector('#scissors-btn');
 
-rockBtn.addEventListener('click', function() {
-  console.log("clicked rock");
+// Display Divs
+const displayPlayer = document.querySelector('#display-player');
+const displayComputer = document.querySelector('#display-computer');
+const displayResult = document.querySelector('#display-result');
+
+rockBtn.addEventListener('click', () => {
+  displayPlayer.textContent = "Player chose Rock";
 });
 
-paperBtn.addEventListener('click', function() {
-  console.log("clicked paper");
+paperBtn.addEventListener('click', () => {
+  displayPlayer.textContent = "Player chose Paper";
 });
 
-scissorsBtn.addEventListener('click', function() {
-  console.log("clicked scissors");
+scissorsBtn.addEventListener('click', () => {
+  displayPlayer.textContent = "Player chose Scissors";
 });
+
+// After you press the button, you should start the game
+const choices = ["rock", "paper", "scissors"];
+let randomIndex = Math.floor( Math.random() * 3);
+
