3c5b7de90f1a5b026001aadebe2066466b5d2f4f
sparky9043 <sparky9043@gmail.com>
2024-02-08 11:50:51 +0900

    Add new variables, expand playGame function

--- index.js
+++ index.js
@@ -8,19 +8,45 @@ const displayPlayer = document.querySelector('#display-player');
 const displayComputer = document.querySelector('#display-computer');
 const displayResult = document.querySelector('#display-result');
 
+// Variables and arrays
+const choices = ["rock", "paper", "scissors"];
+let humanSelection;
+let computerSelection;
+let win = 0;
+let lose = 0;
+let tie = 0;
+
+// button event listeners
 rockBtn.addEventListener('click', () => {
   displayPlayer.textContent = "Player chose Rock";
+  humanSelection = "rock";
+  playGame();
 });
 
 paperBtn.addEventListener('click', () => {
   displayPlayer.textContent = "Player chose Paper";
+  humanSelection = "paper";
+  playGame();
 });
 
 scissorsBtn.addEventListener('click', () => {
   displayPlayer.textContent = "Player chose Scissors";
+  humanSelection = "scissors";
+  playGame();
 });
 
 // After you press the button, you should start the game
-const choices = ["rock", "paper", "scissors"];
-let randomIndex = Math.floor( Math.random() * 3);
 
+const playGame = () => {
+  const randomIndex = Math.floor( Math.random() * 3);
+  const random = choices[randomIndex];
+  displayComputer.textContent = `Computer chose ${random}`;
+  computerSelection = random;
+  checkWinner(humanSelection, computerSelection);
+}
+
+function checkWinner(humanSelection, computerSelection) {
+  if ( humanSelection === computerSelection ) {
+    console.log("It's a tie!");
+  }
+}
